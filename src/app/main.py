7fee18385483c1c6f6846cc18f06796ecf29e4d0
jossefaz <jossefaz@protonmail.com>
2021-03-17 16:58:04 +0200

    full CRUS tested

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 
+from .api import ping, notes
+from .database import engine, metadata, database
+
+metadata.create_all(engine)
+
 app = FastAPI()
 
-@app.get("/ping")
-def pong():
-    return {"ping":"pong"}
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+app.include_router(ping.router)
+app.include_router(notes.router, prefix="/notes", tags=["notes"])
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,9 +1,5 @@
-from starlette.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-def test_ping():
-    response = client.get("/ping")
+def test_ping(test_app):
+    response = test_app.get("/ping")
     assert response.status_code == 200
-    assert response.json() == {"ping":"pong"}
+    assert response.json() == {"ping": "pong"}
+
